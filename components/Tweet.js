a0804da8715f32710688015c6e9b3a2ae22bf6c1
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-06-01 12:38:09 +0530

    updated settings

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,12 +1,12 @@
-const Tweet = ({ bg, cardColor, opacity }) => {
+const Tweet = ({ bg, cardColor, opacity, scale }) => {
   return (
     <div
-      className="md:w-1/2 md:h-4/6 w-5/6 h-2/5 ml-auto mr-auto md:ml-20 rounded-3xl p-3 flex justify-center items-center"
+      className="md:w-1/2 md:h-4/6 w-5/6 h-2/5 ml-auto mr-auto md:ml-20 rounded-3xl p-10 flex justify-center items-center"
       style={{ background: bg }}
     >
       <div
         id="tweet-card"
-        className="md:w-3/4 md:h-4/6 w-5/6 h-3/4 rounded-3xl p-3"
+        className="md:w-3/4 md:h-4/6 w-5/6 h-3/4 rounded-3xl p-3 scale-125"
         style={{ background: cardColor, opacity: opacity }}
       ></div>
     </div>
