660201ca5c155a4a4c91b26140fe514ef4a01a33
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-06-02 19:39:49 +0530

    added text to logo

--- components/LogoChange.js
+++ components/LogoChange.js
@@ -9,13 +9,19 @@ export const LogoChange = () => {
   if (currentTheme === "light") {
     return (
       <div className="flex items-center justify-between mb-0 ml-4 transition-all duration-200">
-        <a className="p-0" href="/">
+        <a className="p-0 inline-flex items-center justify-center" href="/">
           <Image
             src={logoLight}
             alt="TweetSnap Logo Light"
             width={55}
             height={55}
           />
+          <span
+            id="logo-title"
+            className="ml-3 font-extrabold text-3xl text-dark"
+          >
+            TweetSnap
+          </span>{" "}
         </a>
       </div>
     );
@@ -29,6 +35,12 @@ export const LogoChange = () => {
             width={55}
             height={55}
           />
+          <span
+            id="logo-title"
+            className="ml-3 font-extrabold text-3xl text-light"
+          >
+            TweetSnap
+          </span>
         </a>
       </div>
     );
