72c51e1965e14e05c636ce31a643a22431b098cc
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-06-08 08:01:16 +0530

    fixed logoChange bug caused by checking for light theme first - changed to check for dark theme first

--- components/LogoChange.js
+++ components/LogoChange.js
@@ -5,27 +5,8 @@ import logoDark from "../public/LogoDark.svg";
 
 export const LogoChange = () => {
   const { systemTheme, theme } = useTheme();
-  const currentTheme = theme === "system" ? systemTheme : theme;
-  if (currentTheme === "light") {
-    return (
-      <div className="flex items-center justify-between mb-0 ml-4 transition-all duration-300">
-        <a className="p-0 inline-flex items-center justify-center" href="/">
-          <Image
-            src={logoLight}
-            alt="TweetSnap Logo Light"
-            width={55}
-            height={55}
-          />
-          <span
-            id="logo-title"
-            className="ml-3 font-extrabold text-3xl text-dark"
-          >
-            TweetSnap
-          </span>
-        </a>
-      </div>
-    );
-  } else {
+  const currentTheme = theme;
+  if (currentTheme === "dark") {
     return (
       <div className="flex items-center justify-between mb-0 ml-4 transition-all duration-300">
         <a className="p-0 inline-flex items-center justify-center" href="/">
@@ -38,6 +19,25 @@ export const LogoChange = () => {
           <span
             id="logo-title"
             className="ml-3 font-extrabold text-3xl text-light"
+          >
+            TweetSnap
+          </span>
+        </a>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex items-center justify-between mb-0 ml-4 transition-all duration-300">
+        <a className="p-0 inline-flex items-center justify-center" href="/">
+          <Image
+            src={logoLight}
+            alt="TweetSnap Logo Light"
+            width={55}
+            height={55}
+          />
+          <span
+            id="logo-title"
+            className="ml-3 font-extrabold text-3xl text-dark"
           >
             TweetSnap
           </span>
